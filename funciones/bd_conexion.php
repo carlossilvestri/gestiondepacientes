490852b5bc7cec56e8b411de0d3dbd4d8aef4beb
carlossilvestri <54336195+carlossilvestri@users.noreply.github.com>
2020-11-29 15:02:44 -0400

    Update bd_conexion.php

--- funciones/bd_conexion.php
+++ funciones/bd_conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'pacientes'); //Servidor, usuario, contraseña, nombre de la base de datos.
+$conn = new mysqli('localhost', 'u268832824_carlossilvestr', 'Kar2La29%', 'u268832824_pacientes'); //Servidor, usuario, contraseña, nombre de la base de datos.
 mysqli_set_charset($conn, 'utf8');  //Coloca UTF8 en PHP (SIN ESTO no puedo colocar acentos ni ñ).
 if ($conn->connect_error) {
     echo $error->$conn->connect_error;
