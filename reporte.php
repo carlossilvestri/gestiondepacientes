19b32d77e7c35b4d4bca96fbfd983699f7a8fcbf
carlossilvestri <54336195+carlossilvestri@users.noreply.github.com>
2020-11-30 09:53:41 -0400

    Update reporte.php

--- reporte.php
+++ reporte.php
@@ -70,8 +70,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'av85699@gmail.com';                     // SMTP username
-    $mail->Password   = 'ventura9';                               // SMTP password
+    $mail->Username   = 'tucorreo@gmail.com';                     // SMTP username
+    $mail->Password   = 'tucontraseña';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
