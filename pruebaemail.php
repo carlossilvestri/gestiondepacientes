1c7067e125c786850bbc79b8240b45eb38304029
carlossilvestri <54336195+carlossilvestri@users.noreply.github.com>
2020-11-30 09:55:14 -0400

    Update pruebaemail.php

--- pruebaemail.php
+++ pruebaemail.php
@@ -16,14 +16,14 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'av85699@gmail.com';                     // SMTP username
-    $mail->Password   = 'ventura9';                               // SMTP password
+    $mail->Username   = 'tucorreo@gmail.com';                     // SMTP username
+    $mail->Password   = 'tucontraseña';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('av85699@gmail.com', 'Adm de Examenes');
-    $mail->addAddress('carlossilvestri9@gmail.com', 'Carlos Silvestri');     // Add a recipient
+    $mail->setFrom('tucorrel@gmail.com', 'Adm de Examenes');
+    $mail->addAddress('correoquienvaarecibir@gmail.com', 'Carlos Silvestri');     // Add a recipient
 
 
     // Attachments
